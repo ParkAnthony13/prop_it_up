7f94652d042fd9e424227d3319441cf23f4765b3
ParkAnthony13 <anth0nypark13@gmail.com>
2021-07-07 20:50:52 -0700

    updated to putting it together

--- src/components/PersonCard.js
+++ src/components/PersonCard.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
 
 class PersonCard extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            count: this.props.age
+        }
+    }
     render() {
         return(
             < div className="Card">
-                <h1>{this.props.firstName}, {this.props.lastName}</h1>
-                <p>Age: {this.props.age}</p>
+                <h1>{this.props.lastName}, {this.props.firstName}</h1>
+                <p>Age: {this.state.count}</p>
                 <p>Hair Color: {this.props.hairColor}</p>
+                <button onClick={ () => this.setState({ count: this.state.count + 1 }) }>Birthday Button for {this.props.firstName} {this.props.lastName}</button>
             </div>
         )
     }
